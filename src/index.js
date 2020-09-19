733a0d3d928ec4b64cdb7b576b6f4689ec86e8b7
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2020-09-19 04:01:53 +0300

    Add webpack support for babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./style.scss";
-console.log('src/index.js')
+console.log('src/index.js');
+
+const p = document.createElement('p');
+p.textContent = 'Hello from webpack!';
+document.body.appendChild(p);
+
+const p2 = document.createElement('p');
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const numbers2 = [7, 8, 9, 10];
+const numbers3 = [...numbers1, ...numbers2];
+p2.textContent = numbers3.join(' ');
+document.body.appendChild(p2);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
       }
