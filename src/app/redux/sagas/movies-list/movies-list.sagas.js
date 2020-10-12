b49aa934bf5e3d90ce0eeaec7bd52abe9b91025c
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2020-10-12 21:36:27 +0300

    Move fetch error throwing into "ApiService.js".

--- src/app/redux/sagas/movies-list/movies-list.sagas.js
+++ src/app/redux/sagas/movies-list/movies-list.sagas.js
@@ -4,9 +4,9 @@ import { put, takeEvery, all } from "redux-saga/effects";
 import ApiService from 'app_services/ApiService';
 import { actionKeys, asyncActionMaps } from 'app_redux/sagas/movies-list/movies-list.actions';
 import { DEFAULT_MOVIES_TYPE } from 'app_redux/sagas/movies-list/movies-list.reducers';
-// import * as apiMovies from "@/api/apiMovies";
 
-// watch
+
+// watchers
 export function* watchMovieBrowser() {
   console.warn('-- sagas/movies-list/movies-list.sagas.js, *watchMovieBrowser()');
   yield all([
@@ -15,7 +15,7 @@ export function* watchMovieBrowser() {
   ]);
 }
 
-// sagas
+// workers
 function* getMoviesSaga({
   type,
   moviesType = DEFAULT_MOVIES_TYPE,
@@ -27,17 +27,11 @@ function* getMoviesSaga({
 
   yield put(actions.start({ moviesType, page }));
   try {
-    const response = yield ApiService.fetch({
+    const data = yield ApiService.fetch({
       useMoviesApi: true,
       url: `/movie/${moviesType}`,
       urlParams: `&page=${page}`
     });
-
-    let data = response.ok
-      ? yield response.json()
-      : yield response.text()
-        .then(text => { throw new Error(text) })
-
     yield put(actions.success(data));
   } catch (error) {
     yield put(actions.fail(error.message));
@@ -51,16 +45,10 @@ function* getGenresSaga({ type }) {
 
   yield put(actions.start());
   try {
-    const response = yield ApiService.fetch({ 
+    const data = yield ApiService.fetch({
       useMoviesApi: true,
-      url: '/genre/movie/list' 
+      url: '/genre/movie/list'
     });
-
-    let data = response.ok
-      ? yield response.json()
-      : yield response.text()
-        .then(text => { throw new Error(text) })
-    console.warn('-- sagas/movies-list/movies-list.sagas.js, *getGenresSaga(), data:', data);
     yield put(actions.success(data.genres));
   } catch (error) {
     yield put(actions.fail(error.message));
--- src/app/services/ApiService.js
+++ src/app/services/ApiService.js
@@ -6,20 +6,40 @@ import {
   TMDB_LANGUAGE
 } from 'app_config';
 
+
+function* fetchJson(url, { method }) {
+  const response = yield fetch(url, { method });
+
+  if (response.ok) {
+    return yield response.json()
+  } else {
+    yield response
+      .text()
+      .then(text => { throw new Error(text) })
+  }
+};
+
 export default {
   fetch: function ({
     useMoviesApi = false,
     url = '',
     urlParams = ''
   }) {
-    const fetchUrl = useMoviesApi
-      ? `${TMDB_API_HOST}${url}?api_key=${TMDB_API_KEY}&${TMDB_LANGUAGE}${urlParams}`
-      : `${process.env.API_PATH}${url}`;
+    let fetchUrl;
+    let fetchFunc;
+    const fetchParams = { method: 'GET' };
+
+    if (useMoviesApi) {
+      fetchUrl = `${TMDB_API_HOST}${url}?api_key=${TMDB_API_KEY}&${TMDB_LANGUAGE}${urlParams}`;
+      fetchFunc = fetchJson;
+
+    } else {
+      fetchUrl = `${process.env.API_PATH}${url}`;
+      fetchFunc = fetch;
+    }
 
     console.log(`\n-- ApiService, fetchUrl: ${fetchUrl}`);
 
-    return fetch(fetchUrl, {
-      method: 'GET'
-    });
+    return fetchFunc(fetchUrl, fetchParams);
   }
 };
