9b39c8a1dfc398a7b9ceca95f5f46901a5689286
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2020-10-24 02:34:48 +0300

    Styling for movies list pagination.

--- src/app/components/pages/movies-page/_blocks/PagingBlock/PagingBlock.jsx
+++ src/app/components/pages/movies-page/_blocks/PagingBlock/PagingBlock.jsx
@@ -2,11 +2,16 @@ import './PagingBlock.scss';
 
 import React from 'react';
 import PT from 'prop-types';
+import b_ from 'b_';
+import cn from 'classnames';
 import ReactPaginate from "react-paginate";
 
 import PTS from 'app_services/PropTypesService';
 
 function PagingBlock(props) {
+  const cls_base = 'pagination';
+  const b = b_.B({ modSeparator: '--' }).with(cls_base);
+
   return (
     <ReactPaginate
       previousLabel="&larr;"
@@ -20,9 +25,12 @@ function PagingBlock(props) {
       disableInitialCallback={true}
       initialPage={props.initialPage}
       forcePage={props.initialPage}
-      containerClassName="pagination m-0"
-      pageLinkClassName="pagination__link"
+      containerClassName={cn(b(), 'm-0')}
       activeClassName="active"
+      // we need specify that classes because of bootstrap reboot styles (for not to override them)
+      pageLinkClassName={b('link')}
+      previousLinkClassName={b('link', { prev: true })}
+      nextLinkClassName={b('link', { next: true })}
     />
   );
 };
