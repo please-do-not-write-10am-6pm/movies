74684423c3a7b26ee7d98023c5ff43c67707d308
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2020-10-24 03:45:32 +0300

    Refactoring for ListBlock (move error and loading messages to MoviePage). Wrap Paging block into "has movies" data check.

--- src/app/components/pages/movies-page/MoviesPage/MoviesPage.jsx
+++ src/app/components/pages/movies-page/MoviesPage/MoviesPage.jsx
@@ -1,14 +1,16 @@
 import './MoviesPage.scss';
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PT from 'prop-types';
 import b_ from 'b_';
 
 import PTS from 'app_services/PropTypesService';
+import { isNotEmpty } from 'app_services/UtilsService';
 import { Row, Column } from 'app_components/layout';
 import { MListContextProvider } from 'app_contexts/MListContext';
 import { ToolbarBlock, PagingBlock, ListBlock } from
   'app_components/pages/movies-page/_blocks';
+import { ProgressBar } from 'app_components/layout';
 
 function MoviesPage({ data_toolbar, data_paging, data_genresContext, data_moviesList }) {
 
@@ -16,19 +18,31 @@ function MoviesPage({ data_toolbar, data_paging, data_genresContext, data_movies
   const cls_base = 'movies-list';
   const b = b_.with(cls_base);
 
+  const { isLoading, error, movies } = data_moviesList;
+  const hasMovies = isNotEmpty(movies);
+
   return (
     <div className={b()}>
+
       <Row cls="mb-2">
         <ToolbarBlock {...data_toolbar} />
       </Row>
 
-      <Row cls="mb-2">
-        <PagingBlock {...data_paging} />
-      </Row>
+      {hasMovies
+        ? <Row cls="mb-2">
+          <PagingBlock {...data_paging} />
+        </Row>
+        : ''}
 
       <MListContextProvider {...data_genresContext}>
         <Row>
-          <ListBlock {...data_moviesList} />
+          {error && <p>{error}</p>}
+
+          {isLoading && <ProgressBar />}
+
+          {hasMovies
+            ? <ListBlock movies={movies} />
+            : ''}
         </Row>
       </MListContextProvider>
     </div>
--- src/app/components/pages/movies-page/_blocks/ListBlock/ListBlock.jsx
+++ src/app/components/pages/movies-page/_blocks/ListBlock/ListBlock.jsx
@@ -1,42 +1,24 @@
 import './ListBlock.scss';
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PT from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-import PTS from 'app_services/PropTypesService';
-import { isNotEmpty } from 'app_services/UtilsService';
 import { CardBlock } from 'app_components/pages/movies-page/_blocks';
-import { ProgressBar } from 'app_components/layout';
 
 function ListBlock(props) {
-  const { isLoading, error, movies } = props;
-
   return (
-    <Fragment>
-      {error && <p>{error}</p>}
-
-      {isLoading && <ProgressBar />}
-
-      {
-        isNotEmpty(movies)
-          ? <div className="d-flex flex-wrap justify-content-between mt-3">
-            {movies.map((movie) =>
-              <CardBlock
-                key={uuidv4()}
-                movie={movie}
-              />
-            )}
-          </div>
-          : ''
-      }
-    </Fragment >
+    <div className="d-flex flex-wrap justify-content-between mt-3">
+      {props.movies.map((movie) =>
+        <CardBlock
+          key={uuidv4()}
+          movie={movie}
+        />)}
+    </div>
   );
 };
 
 ListBlock.propTypes = {
-  isLoading: PT.bool.isRequired,
-  error: PTS.nullOrString,
   movies: PT.array
 };
 
