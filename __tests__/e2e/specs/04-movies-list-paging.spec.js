fe89aa6d81de9bef3b871f8d57af3ac9d8e032a1
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2021-09-08 16:57:28 +0300

    (tests, e2e, movies list) Improved movies list pagination stability (added active button checks before next step).

--- __tests__/e2e/specs/04-movies-list-paging.spec.js
+++ __tests__/e2e/specs/04-movies-list-paging.spec.js
@@ -2,42 +2,60 @@ import { MoviesListPage as MLP } from '../page-objects';
 
 describe('4) MOVIES LIST: PAGING', () => {
 
-  it('4.1.1: Page number can be changed by clicking page 2 button', () => {
+  it('4.1.1: Page 1 button is active by default', () => {
     cy.visit(`${Cypress.env('homeUrl')}?moviesType=top_rated&page=1`);
-    MLP.listCard().should('have.length', 20);
+    MLP.paginationLinkActive().should('contain', 1);
+  });
 
+  it('4.1.2: First movie card of page 1 has title, genres and rating', () => {
+    MLP.checkFirstMovieCard();
+  });
+
+  it('4.2.1: Page number can be changed by clicking page 2 button', () => {
     MLP.clickPageButton(2);
   });
 
-  it('4.1.2: Url has changed by clicking page 2 button', () => {
+  it('4.2.2: Url has changed by clicking page 2 button', () => {
     cy.url().should('include', 'page=2');
   });
 
-  it('4.1.3: First movie card of page 2 has title, genres and rating', () => {
+  it('4.2.3: Page 2 button is active', () => {
+    MLP.paginationLinkActive().should('contain', 2);
+  });
+
+  it('4.2.4: First movie card of page 2 has title, genres and rating', () => {
     MLP.checkFirstMovieCard();
   });
 
-  it('4.2.1: Page number can be changed by clicking "next" page button', () => {
+  it('4.3.1: Page number can be changed by clicking "next" page button', () => {
     MLP.clickPageNext();
   });
 
-  it('4.2.2: Url has changed by clicking "next" page button', () => {
+  it('4.3.2: Url has changed by clicking "next" page button', () => {
     cy.url().should('include', 'page=3');
   });
 
-  it('4.2.3: First movie card of page 3 has title, genres and rating', () => {
+  it('4.3.3: Page 3 button is active', () => {
+    MLP.paginationLinkActive().should('contain', 3);
+  });
+
+  it('4.3.4: First movie card of page 3 has title, genres and rating', () => {
     MLP.checkFirstMovieCard();
   });
 
-  it('4.3.1: Page number can be changed by clicking "..." page button', () => {
+  it('4.4.1: Page number can be changed by clicking "..." page button', () => {
     MLP.clickBreakPage();
   });
 
-  it('4.3.2: Url has changed by clicking "..." page button', () => {
+  it('4.4.2: Url has changed by clicking "..." page button', () => {
     cy.url().should('include', 'page=5');
   });
 
-  it('4.3.3: First movie card of page 5 has title, genres and rating', () => {
+  it('4.4.3: Page 5 button is active', () => {
+    MLP.paginationLinkActive().should('contain', 5);
+  });
+
+  it('4.4.4: First movie card of page 5 has title, genres and rating', () => {
     MLP.checkFirstMovieCard();
   });
 })
