b3dfd8cd731e29544f2f71267fbf4ca13ef639b0
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2021-09-16 12:21:57 +0300

    (env) Added explicit build logs for source hash value.

--- configs/env/source-hash/index.js
+++ configs/env/source-hash/index.js
@@ -1,8 +1,23 @@
 const gitHash = require('./git-hash');
 
 const { env } = process;
+const { SOURCE_HASH } = env;
 
-let sourceHash = (gitHash || env.SOURCE_HASH);
+let sourceHash = '';
+
+/* eslint-disable no-console */
+if (gitHash) {
+  console.log('--- Source hash: using value from Git hash');
+  sourceHash = gitHash;
+
+} else if (SOURCE_HASH) {
+  console.log(`--- Source hash: using value from process.env.SOURCE_HASH: ${SOURCE_HASH}`);
+  sourceHash = SOURCE_HASH;
+
+} else {
+  console.log('--- Source hash: no value');
+}
+/* eslint-disable no-console */
 
 if (typeof sourceHash.substring === 'function') {
   sourceHash = sourceHash.substring(0, 7);
